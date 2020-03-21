404bf8bf7950ff44e24311a87205cfe373562d94
xiaobaix25@gmail.com <57908497+xiaobai-myh123@users.noreply.github.com>
2020-03-21 22:02:08 +0800

    为什么没有啊推送成功啊

--- src/cn/whcm/filter/MyTestGit.java
+++ src/cn/whcm/filter/MyTestGit.java
@@ -2,7 +2,7 @@ package cn.whcm.filter;
 public class MyTestGit {
 	public static void main(String[] args) {
 		
-		System.out.println("我是测试推送远程库的111");
+		System.out.println("111我是测试推送远程库的111");
 		
 	}
 }
