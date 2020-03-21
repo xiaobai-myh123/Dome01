1eb5197f85866d9b82aa21bec6a78542c1309ddf
xiaobaix25@gmail.com <57908497+xiaobai-myh123@users.noreply.github.com>
2020-03-21 21:59:10 +0800

    我是为了test推送第二次

--- src/cn/whcm/filter/MyTestGit.java
+++ src/cn/whcm/filter/MyTestGit.java
@@ -2,7 +2,7 @@ package cn.whcm.filter;
 public class MyTestGit {
 	public static void main(String[] args) {
 		
-		System.out.println("我是测试推送远程库的");
+		System.out.println("我是测试推送远程库的111");
 		
 	}
 }
